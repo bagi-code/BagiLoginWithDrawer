daa283cc3e662b5a65ecc6ee044ed04efcfeb273
bagicode <rdp10007@gmail.com>
2017-04-29 06:17:29 +0700

    Initial commit

---
+++ app/src/main/java/com/bagicode/www/bagiloginwithdrawer/LoginActivity.java
@@ -0,0 +1,23 @@
+package com.bagicode.www.bagiloginwithdrawer;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+public class LoginActivity extends AppCompatActivity  {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+    }
+
+    public void goMain(View v){
+        Intent goMain = new Intent(LoginActivity.this, MainActivity.class);
+        startActivity(goMain);
+    }
+
+
+}
